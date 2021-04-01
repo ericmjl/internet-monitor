276bd2be220745cfab1c7003c199d73255182d00
Eric Ma <ericmajinglong@gmail.com>
2021-04-01 15:52:51 -0400

    Ensure only a singleton instance of the script runs per machine

--- recorder.py
+++ recorder.py
@@ -1,5 +1,8 @@
 import schedule
 from lib import record_data
+from tendo import singleton
+
+me = singleton.SingleInstance()
 
 # Program the schedule
 schedule.every(1).to(2).minutes.do(record_data)
