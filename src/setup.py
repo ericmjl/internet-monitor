f4268c76c556938d2c7bb68364ff2c34a598a5dd
Eric Ma <ericmajinglong@gmail.com>
2021-04-02 18:35:14 -0400

    Add long description

--- src/setup.py
+++ src/setup.py
@@ -1,10 +1,19 @@
 import setuptools
+from pathlib import Path
+
+
+root_dir = Path(__file__).parent.parent.absolute()
+readme_path = root_dir / "README.md"
+
+with open(readme_path, "r+") as f:
+    readme = f.read()
 
 setuptools.setup(
     name="netspeedmonitor",
-    version="0.1.0",
+    version="0.1.1",
     description="A utility for recording your internet speed.",
-    long_description="",
+    long_description=readme,
+    long_description_content_type="text/markdown",
     url="https://github.com/ericmjl/internet-monitor",
     author="Eric J. Ma",
     author_email="ericmajinglong@gmail.com",
