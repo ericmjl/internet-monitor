c52a78472db502beb82c6bd31280ba3a053bb09c
Eric Ma <ericmajinglong@gmail.com>
2021-04-06 18:37:57 -0400

    Simplified UI

--- src/netspeedmonitor/ui.py
+++ src/netspeedmonitor/ui.py
@@ -1,40 +1,25 @@
 """Streamlit UI."""
 import socket
-from functools import partial
-from threading import Thread
 
-import pandas as pd
+import matplotlib.pyplot as plt
 import streamlit as st
 from tendo import singleton
 
-from netspeedmonitor.speedtest import (
-    load_db,
-    log_data,
-    measure_speed,
-    record_func,
-    to_dataframe,
-)
+from netspeedmonitor.database import table_to_df
+from netspeedmonitor.latency import measure_local, measure_sentinels
+from netspeedmonitor.speedtest import load_db, measure_netspeed
+
+
+def despine(ax):
+    ax.spines["right"].set_visible(False)
+    ax.spines["top"].set_visible(False)
+
 
 me = singleton.SingleInstance(lockfile="/tmp/netspeedmonitor_ui.lock")
 
-
 db = load_db()
 
-
-def load_dataframe():
-    df = to_dataframe(db)
-    df = df.rename(
-        {
-            "download_speed": "Download Speed (Mbps)",
-            "upload_speed": "Upload Speed (Mbps)",
-        },
-        axis=1,
-    )
-    return df
-
-
 st.header("Internet Speed Monitor")
-
 
 hostname = socket.gethostname()
 st.write(
@@ -44,67 +29,57 @@ st.write(
 """
 )
 
-plot = st.empty()
-plot.line_chart(data=load_dataframe())
+
+chart_col1, chart_col2 = st.beta_columns([1, 1])
+
+with chart_col1:
+    speed_chart = st.empty()
+
+with chart_col2:
+    latency_chart = st.empty()
 
 
-col1, col2, col3 = st.beta_columns([1, 1, 1])
-# Allow output mutation is necessary
-# so that we don't overwrite running_threads
-# each time an interaction happens on the UI.
-@st.cache(allow_output_mutation=True)
-def init_running_processes():
-    return []
+def refresh_speed_chart():
+    fig, ax = plt.subplots(figsize=(5, 2))
+    df = table_to_df("speed").set_index("datetime")
+    df.plot(ax=ax)
+    despine(ax)
+    speed_chart.pyplot(fig)
 
 
-running_processes = init_running_processes()
-with col1:
-    st.write("Background recording")
-    status = st.empty()
-    if len(running_processes) == 0:
-        min_interval_placeholder = st.empty()
-        min_interval = min_interval_placeholder.number_input(
-            "Minimum Interval (minutes)", min_value=1, max_value=60
-        )
-        max_interval_placeholder = st.empty()
-        max_interval = max_interval_placeholder.number_input(
-            "Maximum Interval (minutes)", min_value=2, max_value=120
-        )
-        recorder_placeholder = st.empty()
-        record = recorder_placeholder.button("Start recorder")
-        func = partial(
-            record_func, min_interval=min_interval, max_interval=max_interval
-        )
-        if record:
-            process = Thread(target=func, daemon=True)
-            process.start()
-            running_processes.append(process)
-            status.success(f"Recorder running in background")
-            recorder_placeholder.empty()
-            min_interval_placeholder.empty()
-            max_interval_placeholder.empty()
-
-    else:
-        status.success(f"Recorder running in background")
+def refresh_latency_chart():
+    df = table_to_df("latency")
+    piv = df.pivot_table(values="latency", columns=("host"), index="datetime")
+    fig, ax = plt.subplots(figsize=(5, 2))
+    for column in piv.columns:
+        piv[column].dropna().plot(ax=ax, label=column)
+    ax.legend()
+    despine(ax)
+    latency_chart.pyplot(fig)
 
 
-with col2:
-    st.write("Measure internet speed once.")
-    measure = st.button("Measure")
-    if measure:
-        with st.spinner("Measuring..."):
-            data = measure_speed()
-            st.write(data)
-            log_data(data, db)
+refresh_speed_chart()
+refresh_latency_chart()
 
-with col3:
-    st.write("Refresh the chart.")
-    refresh = st.button("Refresh")
-    if refresh:
-        plot.line_chart(data=load_dataframe())
+
+st.sidebar.header("Control Panel")
+measure = st.sidebar.button("Measure internet speed once")
+
+if measure:
+    with st.spinner("Measuring..."):
+        measure_netspeed()
+        measure_local()
+        measure_sentinels()
+
+refresh = st.sidebar.button("Refresh charts")
+if refresh:
+    refresh_speed_chart()
+    refresh_latency_chart()
 
 
 st.header("Error log")
-errors = db.table("errors")
-errors_df = pd.DataFrame(errors.all())
-st.write(errors_df)
+try:
+    errors_df = table_to_df("errors")
+    st.write(errors_df)
+except Exception:
+    st.write("There are no errors showing up just yet!")
